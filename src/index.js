d99db9f3bfee2f20e170b83a6dc4515ed0126ca7
Jingwen Owen Ou <jingweno@gmail.com>
2015-08-22 09:11:28 -0700

    Rewrite index.js to be ES6 compatible

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-var menubar = require('menubar')
-var shell = require('shell')
-var dialog = require('dialog')
-var Server = require('electron-rpc/server')
+import Menubar from "menubar"
+import Shell   from "shell"
+import Dialog  from "dialog"
+import Server  from "electron-rpc/server"
 
 var server = new Server()
 
 var opts = {dir: __dirname}
-var menu = menubar(opts)
+var menu = Menubar(opts)
 
-process.on('uncaughtException', function (err) {
-  dialog.showErrorBox('Uncaught Exception: ' + err.message, err.stack || '')
+process.on("uncaughtException", function (err) {
+  dialog.showErrorBox("Uncaught Exception: " + err.message, err.stack || "")
   menu.app.quit()
 })
 
-menu.on('ready', function () {
-  menu.on('after-create-window', function () {
+menu.on("ready", function () {
+  menu.on("after-create-window", function () {
     server.configure(menu.window.webContents)
-    menu.window.webContents.on('new-window', function (e, url, frameName, disposition) {
+    menu.window.webContents.on("new-window", function (e, url, frameName, disposition) {
       e.preventDefault()
       shell.openExternal(url)
     })
   })
 
-  server.on('terminate', function (e) {
+  server.on("terminate", function (e) {
     server.destroy()
     menu.app.terminate()
   })
 
-  server.on('open-url', function(req) {
+  server.on("open-url", function(req) {
     shell.openExternal(req.body.url)
   })
 })
