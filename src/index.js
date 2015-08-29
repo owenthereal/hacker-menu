94ff698c75d5c7b44e595f92839b7580af6f2d17
Jingwen Owen Ou <jingweno@gmail.com>
2015-08-28 22:07:03 -0700

    Set new story icon when there’re new stories

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import Path from 'path'
 import _ from 'underscore'
 import AutoUpdateManager from './server/auto_update_manager'
 import StoryManager from './server/story_manager'
+import TrayManager from './server/tray_manager'
 import StoryType from './model/story_type'
 
 var server = new Server()
 
-var opts = { dir: __dirname, icon: Path.join(__dirname, '..', 'images', 'Icon.png'), preloadWindow: true }
+var opts = {
+  dir: __dirname,
+  icon: Path.join(__dirname, '..', 'images', 'Icon.png'),
+  iconNew: Path.join(__dirname, '..', 'images', 'Icon-new.png'),
+  preloadWindow: true
+}
 var menu = Menubar(opts)
 
 process.on('uncaughtException', function (error) {
@@ -38,9 +44,14 @@ menu.on('ready', function () {
     server.send('update-available', releaseVersion)
   })
 
+  var trayManager = new TrayManager(menu.window, menu.tray, opts.icon, opts.iconNew)
+
   var storyManager = new StoryManager(20)
   storyManager.on('story-manager-status', function (status) {
     server.send('story-manager-status', status)
+  })
+  storyManager.on('new-story', function () {
+    trayManager.notifyNewStories()
   })
 
   _.each(StoryType.ALL, function (type) {
