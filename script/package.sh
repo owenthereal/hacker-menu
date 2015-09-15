9f49c6721b8f028ffe305f7411ddb4a197360e27
Jingwen Owen Ou <jingweno@gmail.com>
2015-09-15 08:39:23 -0700

    Convert bash package script to be programmable

--- script/package.sh
+++ script/package.sh
@@ -1,16 +1,38 @@
-#!/usr/bin/env bash
+#!/usr/bin/env node
 
-set -e
+var packager = require('electron-packager')
+var package = require('../package.json')
+var opts = {
+  dir: '.',
+  name: 'Hacker Menu',
+  overwrite: true,
+  sign: 'Developer ID Application: Jingwen Ou',
+  icon: 'images/Icon@1024.icns',
+  platform: 'darwin',
+  arch: 'x64',
+  version: package.config.electron_version,
+  ignore: [
+    'src',
+    'script',
+    'release',
+    'node_modules/(babel|standard|csscomb)',
+    'node_modules/electron-(packager|prebuild|rebuild)'
+  ]
+}
+packager(opts, function done (err, appPaths) {
+  if (err) {
+    if (err.message) {
+      console.error(err.message)
+    } else {
+      console.error(err, err.stack)
+    }
 
-electron-packager . 'Hacker Menu' \
-  --overwrite \
-  --sign='Developer ID Application: Jingwen Ou' \
-  --icon='images/Icon@1024.icns' \
-  --platform=darwin \
-  --arch=x64 \
-  --version=$npm_package_config_electron_version \
-  --ignore='src' \
-  --ignore='script' \
-  --ignore='release' \
-  --ignore='node_modules/(babel|standard|csscomb)' \
-  --ignore='node_modules/electron-(packager|prebuild|rebuild)'
+    process.exit(1)
+  }
+
+  if (appPaths.length > 1) {
+    console.error('Wrote new apps to:\n' + appPaths.join('\n'))
+  } else if (appPaths.length === 1) {
+    console.error('Wrote new app to', appPaths[0])
+  }
+})
